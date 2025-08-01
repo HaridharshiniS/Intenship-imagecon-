0e0a305ae18ed75b7bc2088913c83215d992315f
HaridharshiniS <Kamalhari9080@gmail.com>
2025-08-01 22:15:21 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,17 +1,30 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.js'; // Added .js extension
-import reportWebVitals from './reportWebVitals.js'; // Added .js extension
+import Home from './pages/Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin.jsx';
+import Signup from './pages/Signup.jsx';
+import Product from './pages/Product.jsx';
+import Contact from './pages/Contact.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path = "/Signin" element = {<Signin/>}/>
+          <Route path = "/signup" element = {<Signup/>}/>
+          <Route path ="/product" element ={<Product/>}/>
+          <Route path = "/contact" element ={<Contact/>}/>
+          
+         
+          </Routes>
+      </BrowserRouter>
+     
+    
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
